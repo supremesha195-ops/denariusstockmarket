29f23efc919e9e86ed677594328d0dc5e1c6f170
supremesha195-ops <supremesha195@gmail.com>
2025-12-04 16:18:53 +0530

    Create footer.tsx

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "10px", backgroundColor: "#111", color: "#fff", marginTop: "40px", textAlign: "center" }}>
+      <p>© 2025 Denarius Stock Market. All rights reserved.</p>
+    </footer>
+  );
+}
