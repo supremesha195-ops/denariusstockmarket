287565240dab9aa6a66a0429cc6ee19bb67782e5
supremesha195-ops <supremesha195@gmail.com>
2025-12-04 16:19:50 +0530

    Create home.tsx

---
+++ src/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>Welcome to Denarius Stock Market</h1>
+      <p>This is your stock trading simulation platform.</p>
+    </div>
+  );
+}
