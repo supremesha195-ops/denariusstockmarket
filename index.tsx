7f5ebfe514c9d2ac3c5c2a7afe1e3fe403f0df6f
supremesha195-ops <supremesha195@gmail.com>
2025-12-04 16:02:13 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./src/context/ThemeContext";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) {
